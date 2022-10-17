907b143c897787b509d2969a1aed477e9bd97342
Pablo <pmartinez@quimerico.uy>
2022-10-17 15:08:31 -0300

    Api sample and upgrade dependencies

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,23 +1,32 @@
 const { Request, Response } = require("express");
 
 const apiAlive = (req, res) => {
-  const response = {
-    message: "Este es el index",
-  };
-  res.json(response);
+	const response = {
+		message: "Este es el index",
+	};
+	res.json(response);
 };
 
-
 const apiPing = (req, res) => {
-  const { data } = req.body;
-  console.log(data);
-  const response = {
-    data
-  }
-  res.json(response)
-}
+	const { data } = req.body;
+	console.log(data);
+	const response = {
+		data,
+	};
+	res.json(response);
+};
+
+const apiSample = (req, res) => {
+	const { name, description } = req.body;
+	const response = {
+		name,
+		description,
+	};
+	res.json(response);
+};
 
 module.exports = {
-  apiAlive,
-  apiPing
+	apiAlive,
+	apiPing,
+	apiSample,
 };
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const { apiAlive, apiPing } = require("../controller/apiController");
+const { apiAlive, apiPing, apiSample } = require("../controller/apiController");
 
 const router = Router();
 
 router.get("/api/alive", apiAlive);
+
+router.get("/api/sample", apiSample);
 
 router.post("/api/ping", apiPing);
 
